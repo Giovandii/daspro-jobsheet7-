80c3ecf7a0046a6bf20068e8151c87cf5ecc4ff2
mohhamadgiovandisaputra-glitch <mohhamadgiovandisaputra@gmail.com>
2025-10-22 08:44:14 +0700

    perubahan

--- SiakadFor14.java
+++ SiakadFor14.java
@@ -2,12 +2,18 @@ import java.util.Scanner;
 public class SiakadFor14{
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
-//Deklarassi 
-        double nilai, tertinggi=0, terendah=100;
-//menginisialisai, mengkondisikan, dan merubah suatu perulangan
+        //Deklarassi 
+        double nilai=0, tertinggi=0, terendah=100;
+        int lulus=0, tidaklulus=0;
+        //menginisialisai, mengkondisikan, dan merubah suatu perulangan
         for(int i=1; i<= 10; i++){
             System.out.print("Masukan Nilai Siswa Ke-"+i+": ");
             nilai =sc.nextDouble();     //user input nilai
+            if (nilai >= 60) {      //Menentukan lulus atau tidak dengan minimum nilai 60
+                lulus++;
+            } else {
+                tidaklulus++;
+            }
             if(nilai>tertinggi){        //menentukan nilai tertinggi
                 tertinggi=nilai;
             }
@@ -15,7 +21,12 @@ public class SiakadFor14{
                 terendah=nilai;
             }
         }
-        System.out.println("Nilai Tertinggi: "+tertinggi);  //output nilai tertinggi
-        System.out.print("NIlai Terendah: "+terendah);      //output nilai terendah
+        System.out.println("Nilai Tertinggi\t:"+tertinggi);  //output nilai tertinggi
+        System.out.print("NIlai Terendah\t:"+terendah);      //output nilai terendah
+        System.out.println("");
+        
+
+        System.out.println("Jumlah Siswa Yang Lulus\t\t:"+lulus);
+        System.out.println("Jumlah Siswa Yang Tidak Lulus\t:"+tidaklulus);
     }
 }
